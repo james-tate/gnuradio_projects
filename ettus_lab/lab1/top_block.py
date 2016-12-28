0c983f1f1283f8c45228e09ada6b5796b71cb43e
James Tate <jct322@msstate.edu>
2016-12-27 20:13:30 -0600

    added ettus labs

---
+++ ettus_lab/lab1/top_block.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Tue Dec 27 19:28:14 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.var = var = 11
+        self.samp_rate = samp_rate = 1e6
+        self.freq = freq = 1e3
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	label='freq',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	minimum=0,
+        	maximum=16e3,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_freq_sizer)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_c(
+        	self.GetWin(),
+        	title='Scope Plot',
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label='Counts',
+        )
+        self.Add(self.wxgui_scopesink2_0.win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, freq, 1, 0)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_scopesink2_0, 0))
+
+    def get_var(self):
+        return self.var
+
+    def set_var(self, var):
+        self.var = var
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self._freq_slider.set_value(self.freq)
+        self._freq_text_box.set_value(self.freq)
+        self.analog_sig_source_x_0.set_frequency(self.freq)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ettus_lab/lab3/top_block.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Tue Dec 27 19:54:06 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import scopesink2
+from gnuradio.wxgui import waterfallsink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.var = var = 11
+        self.samp_rate = samp_rate = 48e3
+        self.noise_amp = noise_amp = -130
+        self.freq = freq = 1e3
+        self.amp = amp = .5
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _noise_amp_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._noise_amp_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_noise_amp_sizer,
+        	value=self.noise_amp,
+        	callback=self.set_noise_amp,
+        	label='Noise',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._noise_amp_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_noise_amp_sizer,
+        	value=self.noise_amp,
+        	callback=self.set_noise_amp,
+        	minimum=-150,
+        	maximum=0,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_noise_amp_sizer)
+        _freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	label='Frequency',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_freq_sizer,
+        	value=self.freq,
+        	callback=self.set_freq,
+        	minimum=0,
+        	maximum=16e3,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_freq_sizer)
+        _amp_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._amp_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_amp_sizer,
+        	value=self.amp,
+        	callback=self.set_amp,
+        	label='Amp',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._amp_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_amp_sizer,
+        	value=self.amp,
+        	callback=self.set_amp,
+        	minimum=0,
+        	maximum=1,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_amp_sizer)
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_f(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	dynamic_range=100,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=512,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='Waterfall Plot',
+        )
+        self.Add(self.wxgui_waterfallsink2_0.win)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.GetWin(),
+        	title='Scope Plot',
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label='Counts',
+        )
+        self.Add(self.wxgui_scopesink2_0.win)
+        self.blocks_add_xx_0 = blocks.add_vff(1)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, freq, amp, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 10**(1. * noise_amp / 20), 0)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.wxgui_scopesink2_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.wxgui_waterfallsink2_0, 0))
+
+    def get_var(self):
+        return self.var
+
+    def set_var(self, var):
+        self.var = var
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+    def get_noise_amp(self):
+        return self.noise_amp
+
+    def set_noise_amp(self, noise_amp):
+        self.noise_amp = noise_amp
+        self._noise_amp_slider.set_value(self.noise_amp)
+        self._noise_amp_text_box.set_value(self.noise_amp)
+        self.analog_noise_source_x_0.set_amplitude(10**(1. * self.noise_amp / 20))
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self._freq_slider.set_value(self.freq)
+        self._freq_text_box.set_value(self.freq)
+        self.analog_sig_source_x_0.set_frequency(self.freq)
+
+    def get_amp(self):
+        return self.amp
+
+    def set_amp(self, amp):
+        self.amp = amp
+        self._amp_slider.set_value(self.amp)
+        self._amp_text_box.set_value(self.amp)
+        self.analog_sig_source_x_0.set_amplitude(self.amp)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
